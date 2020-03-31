4d1748a706b673fc791360eed1686dbddf94acb5
Alsu7 <44651784+Alsu7@users.noreply.github.com>
2020-03-31 23:15:51 +0400

    Update models.py

--- hello/models.py
+++ hello/models.py
@@ -10,3 +10,20 @@ class LogMessage(models.Model):
         date = timezone.localtime(self.log_date)
         return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
 
+class User(models.Model):
+    login=models.CharField(max_length=50, verbose_name="Логин")
+    password=models.CharField(max_length=30, verbose_name="Пароль")
+    
+class Sights(models.Model):
+    COUNTRY_CHOICES ={
+        (Russia, "Russia"),
+        (Italy, "Italy"),
+        (France, "France"),
+        (Germany, "Germany"),
+        (Norway, "Norway"),
+    }
+    name=models.CharField(max_length=50, verbose_name="Название")
+    price=models.IntegerField()
+    description=models.CharField(max_length=200)
+    country=models.CharField(choises=COUNTRY_CHOICES)
+    year_of_construction=models.IntegerField()
