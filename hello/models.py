c9195cd8bb05946ccad513ee9f228419cffbf840
Alsu7 <44651784+Alsu7@users.noreply.github.com>
2020-05-09 15:09:27 +0400

    Update models.py

--- hello/models.py
+++ hello/models.py
@@ -22,19 +22,16 @@ class Country(models.Model):
                 return u'<a href="{self.im.url}" target="_blank"><img src="{self.im.url}" width="100"/></a>'
             else:
                 return '(Нет изображения)'
-
+    image_img.short_description = 'Картинка'
+    image_img.allow_tags = True
                 
-class Sights(models.Model):
-   
+class Sight(models.Model):
     name=models.CharField(max_length=50, verbose_name="Название")
     price=models.IntegerField()
     description=models.CharField(max_length=200)
-
     country=models.ForeignKey(Country,on_delete=models.CASCADE)
     year_of_construction=models.IntegerField()
     image = models.ImageField(blank=True, upload_to='images', null=True, help_text='150x150px', verbose_name='Ссылка картинки')
-
-
     def __str__(self):
         return f'Название: {self.name}, год постройки: {self.year_of_construction}, цена на вход: {self.price}, страна:{self.country}'
 
