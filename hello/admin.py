1ef708a991f3ee3ceeeec7039e1e4c0ed795dc5c
Alsu7 <44651784+Alsu7@users.noreply.github.com>
2020-05-09 11:55:05 +0400

    Update admin.py

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Sights
+from .models import Country
+
+admin.site.Register(Sights)
+admin.site.Register(Country)
 
 # Register your models here.
