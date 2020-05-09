6677cf8eb61116c884aa087ed8598276457ae561
Alsu7 <44651784+Alsu7@users.noreply.github.com>
2020-05-09 11:55:01 +0400

    Update models.py

--- hello/models.py
+++ hello/models.py
@@ -10,22 +10,27 @@ class LogMessage(models.Model):
         date = timezone.localtime(self.log_date)
         return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
 
+class Country(models.Model):
+    nameCountry=models.CharField(max_length=20)
+    descCountry=models.CharField(max_length=50)
+    im=models.ImageField(blank=True, upload_to='images', null=True, help_text='150x150px', verbose_name='Ссылка картинки страны')
+    def __str__(self):
+        return f'Название: {self.nameCountry}, описание:{self.descCountry}'
 
-    
+    def image_img(self):
+            if self.im:
+                return u'<a href="{self.im.url}" target="_blank"><img src="{self.im.url}" width="100"/></a>'
+            else:
+                return '(Нет изображения)'
+
+                
 class Sights(models.Model):
-    COUNTRY_CHOICES ={
-        ('Russia', "Russia"),
-        ('Italy', "Italy"),
-        ('France', "France"),
-        ('Germany', "Germany"),
-        ('Norway', "Norway")
-
-    }
+   
     name=models.CharField(max_length=50, verbose_name="Название")
     price=models.IntegerField()
     description=models.CharField(max_length=200)
 
-    country=models.CharField(choices=COUNTRY_CHOICES, max_length=50)
+    country=models.ForeignKey(Country,on_delete=models.CASCADE)
     year_of_construction=models.IntegerField()
     image = models.ImageField(blank=True, upload_to='images', null=True, help_text='150x150px', verbose_name='Ссылка картинки')
 
