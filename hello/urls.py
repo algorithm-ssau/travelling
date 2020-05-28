514ee4da2ee65c50ef70b88d96d072c3d9e7b8ed
Elizaveta Kapustina <61021649+lkaiza@users.noreply.github.com>
2020-05-28 22:43:40 +0400

    update html and urls

--- hello/urls.py
+++ hello/urls.py
@@ -3,9 +3,10 @@ from hello import views
 from hello.models import LogMessage
 
 home_list_view = views.HomeListView.as_view(
-    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
-    context_object_name="message_list",
-    template_name="hello/home.html",
+    # queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    # context_object_name="message_list",
+    template_name="hello/main.html",
+    # template_name="hello/home.html",
 )
 
 urlpatterns = [
