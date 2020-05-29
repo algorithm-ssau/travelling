1f163158495a183a041ee94a241c25c0641cbbce
Elizaveta Kapustina <61021649+lkaiza@users.noreply.github.com>
2020-05-29 19:34:50 +0400

    add media_root

--- hello/urls.py
+++ hello/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from hello import views
 from hello.models import LogMessage
 
+
 home_list_view = views.HomeListView.as_view(
     # queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
-    # context_object_name="message_list",
+    context_object_name="message_list",
     template_name="hello/main.html",
     # template_name="hello/home.html",
 )
@@ -14,7 +15,7 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("log/", views.log_message, name="log"),
-    path("", home_list_view, name="home"),
+    path('', home_list_view, name='home'),
     path("main/", views.main, name="main"),
     path("country/<str:country_name>/", views.country, name="country"),
     
--- web_project/urls.py
+++ web_project/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("hello.urls")),
     path("admin/", admin.site.urls),
 ]
 urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
