1bfc1232ea7b0cf5745678bb707cc6eddfcb0f41
Elizaveta Kapustina <61021649+lkaiza@users.noreply.github.com>
2020-05-31 20:03:27 +0400

    deleted unnecessary

--- hello/test.py
+++
@@ -1,5 +0,0 @@
-from models import Sight
-
-
-sights = Sight.objects.get(country = "Германия")
-print(sights)
--- hello/views.py
+++ hello/views.py
@@ -9,15 +9,6 @@ from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 
 
-def hello_there(request, name):
-    return render(
-        request,
-        'hello/hello_there.html',
-        {
-            'name': name,
-            'date': datetime.now()
-        }
-    )
     
 class HomeListView(ListView):
     """Renders the home page, with a list of all messages."""
@@ -27,21 +18,7 @@ class HomeListView(ListView):
         context = super(HomeListView, self).get_context_data(**kwargs)
         return context
 
-# class HomePageView(TemplateView):
 
-#     template_name = "hello/main.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['countries'] = Country.objects.all()
-#         context['sights'] = Sight.objects.all()[:3]
-#         return context
-
-def about(request):
-    return render(request, "hello/about.html")
-
-def contact(request):
-    return render(request, "hello/contact.html")
 
 #create method to render main page
 def main(request):
@@ -49,7 +26,7 @@ def main(request):
 
 #create method to render country page
 def country(request, country_name):
-    country = Country.objects.get(nameCountry = "Австрия")
+    country = Country.objects.get(nameCountry = country_name)
     image_list = [country.im1,
                   country.im2, 
                   country.im3,
@@ -60,22 +37,12 @@ def country(request, country_name):
                   country.im8,
                   country.im9,
                   country.im0]
-    sights = Sight.objects.filter(country = Country.objects.get(nameCountry = country_name))
+    sights = Sight.objects.filter(country = country)
     context = {'country': country,
                'images': image_list,
                'sights': sights
                }   
     return render(request, "hello/country.html", context)
 
-def log_message(request):
-    form = LogMessageForm(request.POST or None)
 
-    if request.method == "POST":
-        if form.is_valid():
-            message = form.save(commit=False)
-            message.log_date = datetime.now()
-            message.save()
-            return redirect("home")
-    else:
-        return render(request, "hello/log_message.html", {"form": form})
 
